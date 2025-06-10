5c46b190469c5c92b26428e9103a4c743a678970
alfredgr42 <alfred2016grigoriev@gmail.com>
2025-06-10 21:52:19 +0300

    Update задание_функционал 1.py

--- задание_функционал 1.py
+++ задание_функционал 1.py
@@ -1,17 +1,15 @@
 def factorial(n):
-if n == 0 or n == 1:
-return 1
+    if n == 0 or n == 1:
+        return 1
 else:
-return n * factorial(n - 1)
-
+    return n * factorial(n - 1)
 def factorials_list(n):
-fact_n = factorial(n)
-print(f"Факториал числа {n} равен: {fact_n}")
-factorials = []
-for i in range(fact_n, 0, -1):
-factorials.append(factorial(i))
-return factorials
-
-number = int(input('Введите число: '))
-result = factorials_list(number)
-print("Список факториалов от", factorial(number), "до 1:", result)
+    fact_n = factorial(n)
+    print(f"Г”Г ГЄГІГ®Г°ГЁГ Г« Г·ГЁГ±Г«Г  {n} Г°Г ГўГҐГ­: {fact_n}")
+    factorials = []
+    for i in range(fact_n, 0, -1):
+        factorials.append(factorial(i))
+        return factorials
+        number = int(input('Г‚ГўГҐГ¤ГЁГІГҐ Г·ГЁГ±Г«Г®: '))
+        result = factorials_list(number)
+        print("Г‘ГЇГЁГ±Г®ГЄ ГґГ ГЄГІГ®Г°ГЁГ Г«Г®Гў Г®ГІ", factorial(number), "Г¤Г® 1:", result)
